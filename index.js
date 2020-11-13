2df8127a7d7811da01f868231fdf0a9c1964dcde
Your Name <you@example.com>
2020-11-13 15:59:23 -0600

    11th - Stretch goal 1 passed

--- index.js
+++ index.js
@@ -98,25 +98,18 @@ const zooAnimals = [
     * The last parameter accepts a callback
     * The consume function should return the invocation of cb, passing a and b into cb as arguments
   */
-  function consume(a, b, cb){
-      return cb(a, b);
-    }
-  
+  const consume = (a, b, cb) => cb(a, b);
+      
   /* 🦁🦁🦁 Step 2: Create several functions to callback with consume(); 🦁🦁🦁 */
  // 🦁🦁🦁 Create a function named add that returns the sum of two numbers 🦁🦁🦁
-  function add(num1, nuum2){
-    return num1 + nuum2;
-  }
+  const add = (num1, num2) => num1 + num2;
+  
 
 // 🦁🦁🦁 Create a function named multiply that returns the product of two numbers 🦁🦁🦁
-  function multiply(num1, num2){
-   return num1 * num2;
-  }
+  const multiply = (num1, num2) => num1 * num2;
 
  // 🦁🦁🦁 Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!" 🦁🦁🦁
-  function greeting(fName, lName){
-   return `Hello ${fName} ${lName}, nice to meet you!`;
-  }
+  const greeting = (fName, lName) => `Hello ${fName} ${lName}, nice to meet you!`;
   
   // 🦁🦁🦁 Step 3: Check your work by un-commenting the following calls to consume(): 🦁🦁🦁 
   // ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️
@@ -126,7 +119,7 @@ const zooAnimals = [
   
   
 // 🦁💪 Stretch: If you haven't already, convert your array method callbacks into arrow functions 🦁💪
- 
+          //DID IT, DONE!!!!
 
 
 
